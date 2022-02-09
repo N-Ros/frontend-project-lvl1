8dd3752d70e6f2866a5467b6326b7bfdb0b0a964
N-Ros <rosinenkova@mail.ru>
2022-02-09 08:55:31 +0300

    add brain-even block

---
+++ bin/brain-even.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+import isEven from '../src/games/brain-even.js';
+
+isEven();
---
+++ src/games/brain-even.js
@@ -0,0 +1,17 @@
+import engine from '../index.js';
+import getRandomNumber from '../getRandomNumber.js';
+
+const gameRules = 'Answer "yes" if the number is even, otherwise answer "no".';
+
+const isEven = (number) => number % 2 === 0;
+
+const gameCheck = () => {
+  const number = getRandomNumber(1, 100);
+  const question = `${number}`;
+  const correctAnswer = isEven(number) ? 'yes' : 'no';
+  return [question, correctAnswer];
+};
+
+const brainEven = () => engine(gameRules, gameCheck);
+
+export default brainEven;
